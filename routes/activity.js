530b6312705dbdeb7213dcb52e73c19f83b3d78c
Yemeskabeba <yemla2006@yahoo.com>
2023-04-28 15:44:15 +0300

    init Commit

--- routes/activity.js
+++ routes/activity.js
@@ -2,6 +2,7 @@ const { v1: Uuidv1 } = require('uuid');
 const JWT = require('../utils/jwtDecoder');
 const SFClient = require('../utils/sfmc-client');
 const logger = require('../utils/logger');
+const encrypt = require('../utils/encrypt')
 
 /**
  * The Journey Builder calls this method for each contact processed by the journey.
@@ -17,16 +18,31 @@ exports.execute = async (req, res) => {
 
   try {
     const id = Uuidv1();
+    const amount = data.inArguments[0].Amount;
+    const fname = data.inArguments[0].Firstname; 
+    const lname= data.inArguments[0].Lastname;
+    const rluid= data.inArguments[0].RelationUid;
+
+    const jsnob = {
+        "amount":{"amount":amount,"currencyCode":"EUR"},
+        "customer":{"firstName": fname,"lastName":lname,"relationUid":rluid}
+       /* "language":language,
+        "productInfo":{"productCode":pcode,"productUid":puid,"referenceNumber":loanId},
+        "tracking":{"countryCode":cc,"entityUid":entity}*/
+        }
+
+      const jsntext = JSON.stringify(jsnob)
+ 
+      const entxt= encrypt.encryptdjsn(jsntext);
 
     await SFClient.saveData(process.env.DATA_EXTENSION_EXTERNAL_KEY, [
       {
         keys: {
           Id: id,
-          SubscriberKey: data.inArguments[0].contactKey,
+          dmCustomerID : data.inArguments[0].contactKey,
         },
         values: {
-          Event: data.inArguments[0].DropdownOptions,
-          Text: data.inArguments[0].Text,
+          encryptedText: entxt,
         },
       },
     ]);
---
+++ utils/encrypt.js
@@ -0,0 +1,12 @@
+const CryptoJS = require("crypto-js");
+
+const encryptdjsn = function(enctext) {
+    // Please parse  your secret key
+//var iv = CryptoJS.enc.Utf8.parse(CryptoJS.lib.WordArray.random(128 / 8));
+    var key = CryptoJS.enc.Utf8.parse("93wj660t8fok9jws");
+var iv = CryptoJS.enc.Utf8.parse("Password");
+    return CryptoJS.AES.encrypt(enctext, key,{
+         iv: iv,padding:CryptoJS.pad.Pkcs7,mode:CryptoJS.mode.CBC 
+    }).ciphertext.toString(CryptoJS.enc.Base64);
+}
+module.exports = encryptdjsn(enctext);
